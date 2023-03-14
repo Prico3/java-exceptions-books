4ea8c9d63d1055e2786bffa5290fb33d40b99f88
Prico3 <pricoco.andrea@gmail.com>
2023-03-14 16:59:30 +0100

    ADD: exceptions

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,9 +1,15 @@
 package org.lessons.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    private final static String FILE_PATH = "./books.txt";
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Book[] books = new Book[0];
@@ -48,6 +54,46 @@ public class Main {
 
         }
 
+        FileWriter myWriter = null;
+        try {
+            myWriter = new FileWriter(FILE_PATH, true);
+            for (Book currentPerson : books) {
+                myWriter.write(currentPerson.toString() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to create file");
+            System.out.println(e.getMessage());
+        } finally {
+            // da qui ci passo sempre e chiudo il FileWriter
+            if (myWriter != null) {
+                try {
+                    myWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-    }
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File(FILE_PATH));
+            while (fileScanner.hasNext()) {
+                String line = fileScanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (fileScanner != null) {
+                try {
+                    fileScanner.close();
+                } catch (IllegalStateException ise) {
+                    ise.printStackTrace();
+                }
+            }
+        }
+
+
+
+        }
 }
