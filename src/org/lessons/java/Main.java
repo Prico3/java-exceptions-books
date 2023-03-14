9ce3f1018ecc0c92556aa790f28650ca9fcda82e
Prico3 <pricoco.andrea@gmail.com>
2023-03-14 15:04:18 +0100

    ADD: created cicle for

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,7 +1,32 @@
 package org.lessons.java;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quanti libri vuoi inserire? ");
+        int n = Integer.parseInt(scan.nextLine());
+        Book[] books = new Book[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Titolo: ");
+            String titolo = scan.nextLine();
+            System.out.println("Numero di pagine: ");
+            int numPagine = Integer.parseInt(scan.nextLine());
+            System.out.println("Autore: ");
+            String autore = scan.nextLine();
+            System.out.println("Editore: ");
+            String editore = scan.nextLine();
+
+            Book newBook = new Book(titolo, numPagine, autore, editore);
+            books[i] = newBook;
+        }
+        scan.close();
+        System.out.println(Arrays.toString(books));
+
 
     }
 }
